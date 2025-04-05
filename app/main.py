f7ecdb38728e5defdad7b32458139293a0ab5f83
Tajay <tajaydrum575@gmail.com>
2025-04-04 21:59:47 -0400

    Added a simple home page

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 from database import create_db_and_tables
 from routers import tasks
+from fastapi.templating import Jinja2Templates
 
 # Lifespan context manager to handle lifecycle
 @asynccontextmanager
@@ -12,14 +16,19 @@ async def lifespan(app: FastAPI):
     yield
     pass
     
+    
 app = FastAPI(lifespan=lifespan, title="Task Manager API")
-
-# Include router without prefix
 app.include_router(tasks.router, tags=["tasks"])
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+BASE_DIR = "app/templates"
+templates = Jinja2Templates(BASE_DIR)
+
 
 @app.get("/")
-def read_root():
-    return {"message": "Welcome to Task Manager API"}
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html",{"request": request})
+
 # start the FastAPI server
 def start_server():
     try:
